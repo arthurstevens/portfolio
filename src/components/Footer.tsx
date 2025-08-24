6a11b3bd80991286ea02352576c9fa72728d0fe2
CustordSolos <arthurstevens228@gmail.com>
2025-08-24 16:55:05 +0100

    feat: add generic footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            &copy; {new Date().getFullYear()} Arthur Srodzinski-Stevens. All rights reserved.
+        </footer>
+    )
+}
+
+export default Footer

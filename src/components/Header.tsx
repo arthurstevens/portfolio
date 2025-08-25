1da2b4c93f180a273905a879201196b1c9f28008
CustordSolos <arthurstevens228@gmail.com>
2025-08-26 00:05:36 +0100

    feat: add social svg icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import CodeIcon from './ui/CodeIcon.tsx'
+import Code from './ui/icons/Code.tsx'
 
 const Header = () => {
   const sections = [
@@ -12,7 +12,7 @@ const Header = () => {
     <header className="fixed top-0 w-full py-4 z-20 bg-background-accent border-b border-border">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <div className="flex space-x-1.5">
-          <CodeIcon className="h-8 w-8 text-accent" />
+          <Code className="h-8 w-8 text-accent" />
           <span className="text-primary font-bold text-xl">Arthur Srodzinski-Stevens</span>
         </div>
         <nav className="w-1/2">

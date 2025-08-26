3b521a15a0f0db02f31a56b6254ffd9f352d9f45
CustordSolos <arthurstevens228@gmail.com>
2025-08-26 23:19:02 +0100

    feat: update social links style

--- src/components/ui/Socials.tsx
+++ src/components/ui/Socials.tsx
@@ -4,28 +4,28 @@ import LinkedIn from "./icons/LinkedIn";
 
 const Socials = () => {
   return (
-    <div className="flex w-min border-1 border-accent/80 hover:border-neutral-100 rounded-md transition duration-300">
+    <div className="group flex h-auto w-min border-t-1 border-b-1 items-center border-accent hover:border-neutral-100 rounded-md transition duration-300">
       <a
         href="https://github.com/arthurstevens/"
         target="_blank"
         rel="noopener noreferrer"
-        className="cursor-pointer px-4 py-2 hover:bg-neutral-100 hover:text-accent hover:shadow-[0_0_15px_theme(colors.primary)] transition-all duration-300 ease-in-out"
+        className="rounded-l-md cursor-pointer px-4 py-2 hover:bg-neutral-100 hover:text-accent hover:shadow-[0_0_15px_theme(colors.primary)] transition-all duration-300 ease-in-out"
       >
         <GitHub size={28} />
       </a>
-      <hr className="border border-dotted h-auto border-accent/50" />
+      <span className="border border-dotted self-stretch border-accent/60 group-hover:border-primary transition duration-300" />
       <a
         href="mailto:arthursrodzisnkistevens@gmail.com"
         className="cursor-pointer px-4 py-2 hover:bg-neutral-100 hover:text-accent hover:shadow-[0_0_15px_theme(colors.primary)] transition-all duration-300 ease-in-out"
       >
         <Email size={28} />
       </a>
-      <hr className="border border-dotted h-auto border-accent/50" />
+      <span className="border border-dotted self-stretch border-accent/60 group-hover:border-primary transition duration-300" />
       <a
         href="https://www.linkedin.com/in/arthur-srodzinski-stevens-2bab09350/"
         target="_blank"
         rel="noopener noreferrer"
-        className="cursor-pointer px-4 py-2 hover:bg-neutral-100 hover:text-accent hover:shadow-[0_0_15px_theme(colors.primary)] transition-all duration-300 ease-in-out"
+        className="rounded-r-md cursor-pointer px-4 py-2 hover:bg-neutral-100 hover:text-accent hover:shadow-[0_0_15px_theme(colors.primary)] transition-all duration-300 ease-in-out"
       >
         <LinkedIn size={28} />
       </a>

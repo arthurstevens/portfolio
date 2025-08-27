1c712939ad82608f91467e35d24aadd8af8a0b8d
CustordSolos <arthurstevens228@gmail.com>
2025-08-27 13:51:38 +0100

    feat(home): add scroll indicator

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import HomeBackground from "./ui/HomeBackground";
 import Socials from "./ui/Socials"
 import Terminal from "./ui/Terminal"
 import Code from "./ui/icons/Code"
+import ArrowDown from "./ui/icons/ArrowDown"
 
 const Home = () => {
   return (
@@ -29,19 +30,28 @@ const Home = () => {
             {/* Projects */}
             <a 
               href="#projects"
-              className="peer/projects order-1 flex-1 basis-0 text-center cursor-pointer flex items-center justify-center p-2 space-x-1 h-auto rounded-md text-black hover:text-primary bg-accent hover:bg-transparent border border-accent hover:border-white transition duration-300 ease-in-out"
+              className="peer/projects order-1 flex-1 basis-0 box-border text-center cursor-pointer flex items-center justify-center p-2 space-x-1 h-auto rounded-md text-black hover:text-primary bg-accent hover:bg-transparent border border-accent hover:border-white transition duration-300 ease-in-out"
             >
               <Code />
               <span className="font-semibold">View Projects</span>
             </a>
             {/* Socials */}
-            <div className="peer/socials order-3 flex-1 basis-0 flex justify-center">
+            <div className="peer/socials order-3 flex-1 basis-0 box-border flex justify-center">
               <Socials />
             </div>
             {/* Divider / line */}
             <span className="order-2 w-15 h-1 border-t-1 border-b-1 border-dashed border-black bg-gradient-to-r from-accent peer-hover/projects:from-primary to-primary/5 peer-hover/socials:to-primary transition duration-300 ease-in-out" />
           </div>
         </div>
+      </div>
+      {/* Scroll */}
+      <div className="absolute bottom-5 flex flex-col justify-center items-center w-full">
+        <a href="#about" className="text-xs text-secondary hover:text-accent">
+          <p>SCROLL</p>
+          <div className="flex animate-bounce justify-center">
+            <ArrowDown size={32}/>
+          </div>
+        </a>
       </div>
     </section>
   );

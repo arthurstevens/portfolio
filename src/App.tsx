9650ce992b537d029fbf92a33a7083104fe4a37f
CustordSolos <arthurstevens228@gmail.com>
2025-08-27 15:28:32 +0100

    feat(about): initialise section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <Home />
+        <About />
       </main>
     </div>
   )
---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <section id="about" className="relative w-full h-screen flex items-center justify-center">
+
+        </section>
+    )
+}
+
+export default About
